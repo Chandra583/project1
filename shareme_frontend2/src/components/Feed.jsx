c32306594f8f89b6f2c94c7735962ee02e94291a
Chandrashekhar gowda KS <106522596+Chandra583@users.noreply.github.com>
2023-01-12 23:32:32 +0530

    thr

--- shareme_frontend2/src/components/Feed.jsx
+++ shareme_frontend2/src/components/Feed.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-function Feed() {
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+const Feed = () => {
+
+  const [pins, setPins] = useState(null);
+  const[loading, setLoading] = useState(true);
+  const { categoryId } = useParams();
+  
+  useEffect(() => {
+    setLoading(true);
+
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+
+    else{
+        client.fetch(feedQuery).then((data)=>{
+          setPins(data);
+          setLoading(true)
+        })
+    }
+
+    
+  }, [categoryId])
+ 
+ 
+
+
+  if (loading) return <Spinner message='we are adding new ideas to your feed!' />
+//   const [pins, setPins] = useState();
+//   const [loading, setLoading] = useState(false);
+//   const { categoryId } = useParams();
+
+//   useEffect(() => {
+//     if (categoryId) {
+//       setLoading(true);
+//       const query = searchQuery(categoryId);
+//       client.fetch(query).then((data) => {
+//         setPins(data);
+//         setLoading(false);
+//       });
+//     } else {
+//       setLoading(true);
+
+//       client.fetch(feedQuery).then((data) => {
+//         setPins(data);
+//         setLoading(false);
+//       });
+//     }
+//   }, [categoryId]);
+//   const ideaName = categoryId || 'new';
+//   if (loading) {
+//     return (
+//       <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+//     );
+//   }
   return (
-    <div>Feed</div>
-  )
-}
+    <div>
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
--- shareme_frontend2/src/container/Home.jsx
+++ shareme_frontend2/src/container/Home.jsx
@@ -11,13 +11,14 @@ import { userQuery } from '../utils/data';
 import { client } from '../client';
 import Pins from './Pins';
 import logo from '../assets/logo.png';
+import { fecthUser } from '../utils/fecthUser';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState();
   const scrollRef = useRef(null);
 
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  const userInfo = fecthUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
